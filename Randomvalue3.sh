42f50e1c66a8d5c3b5128af2841798a50cfd92b8
Gourab kumar Gupta <32832817+GrvSit@users.noreply.github.com>
2022-05-25 20:53:26 +0530

    Randomvalue3.sh recent uploded

--- Randomvalue3.sh
+++ Randomvalue3.sh
@@ -1,33 +1,96 @@
 #!/bin/bash
-
 a=$((RANDOM%900 + 100));
 b=$((RANDOM%900 + 100));
 c=$((RANDOM%900 + 100));
 d=$((RANDOM%900 + 100));
 e=$((RANDOM%900 + 100));
-echo "Random value 1" $((a));
-echo "Random value 2" $((b));
-echo "Random value 3" $((c));
-echo "Random value 4" $((d));
-echo "Random value 5" $((e));
+ echo $a
+ echo $b
+ echo $c
+ echo $d
+ echo $e
+#value for a
+if [[ $a -gt $b && $a -gt $c ]]
+then
+	if [[ $a -gt $d && $a -gt $e ]]
+	then
+	echo "$a is maximum";
+	fi
+fi
 
-i=1
-max=0
+if [[ $a -lt $b && $a -lt $c ]]
+then
+	if [[ $a -lt $d && $a -lt $e ]]
+	then
+	echo "$a is minimum";
+	fi
+fi
 
-echo "Enter Numbers"
-while [ $i -le $N ]
-do
-  read num
-  if [ $i -eq 1 ]  
-  then
-      max=$num
-  else             
-      if [ $num -gt $max ]
-      then
-        max=$num
-      fi
-  fi
-  i=$((i + 1)) 
-done
+#value for b
+if [[ $b -gt $a && $b -gt $c ]]
+then
+   if [[ $b -gt $d && $b -gt $e ]]
+   then
+   echo "$b is maximum";
+   fi
+fi
 
-echo $max
+if [[ $b -lt $a && $b -lt $c ]]
+then
+   if [[ $b -lt $d && $b -lt $e ]]
+   then
+   echo "$b is minimum";
+   fi
+fi
+#value for c
+if [[ $c -gt $a && $c -gt $b ]]
+then
+   if [[ $c -gt $d && $c -gt $e ]]
+   then
+   echo "$c is maximum";
+   fi
+fi
+
+if [[ $c -lt $a && $c -lt $b ]]
+then
+   if [[ $c -lt $d && $c -lt $e ]]
+   then
+   echo "$c is minimum";
+   fi
+fi
+#value for d
+if [[ $d -gt $a && $d -gt $b ]]
+then
+   if [[ $d -gt $c && $d -gt $e ]]
+   then
+   echo "$d is maximum";
+   fi
+fi
+
+if [[ $d -lt $a && $d -lt $b ]]
+then
+   if [[ $d -lt $c && $d -lt $e ]]
+   then
+   echo "$d is minimum";
+   fi
+fi
+#value for e
+if [[ $e -gt $a && $e -gt $b ]]
+then
+   if [[ $e -gt $c && $e -gt $d ]]
+   then
+   echo "$e is maximum";
+   fi
+fi
+
+if [[ $e -lt $a && $e -lt $b ]]
+then
+   if [[ $e -lt $c && $e -lt $d ]]
+   then
+   echo "$e is minimum";
+   fi
+fi
+
+
+
+
